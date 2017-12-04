9f07a06eff1aaffab505c3142e3144d900f0558f
twirler <rmvengen1@buffs.wtamu.edu>
2017-12-04 19:49:25 +0000

    Season and Contestant Data

---
+++ README.md
@@ -0,0 +1,4 @@
+* Survivor api = basically what I am making
+* Make sure to put in detail about my Survivor api and such 
+* Must have an external api (such as weather, mapping, yelp api)
+* Make sure there is angular in the project
---
+++ app_api/routes/index.js
@@ -0,0 +1,9 @@
+var express             = require('express');
+var router              = express.Router();
+var ctrl
+/*
+var ctrlAirplaneData    = require('../controllers/airplane');
+var ctrlAirportData     = require('../controllers/airport');
+var ctrlDarkSkyApi     = require('../controllers/darkskyapi');
+*/
+module.exports = router;
